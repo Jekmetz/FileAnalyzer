a9e39a60e89c6d30122b6724e2cbec05cfab09a1
Jay Kmetz <jekmetz@gmail.com>
2018-03-12 19:03:00 -0400

    Start notes

--- AnlzFile.java
+++ AnlzFile.java
@@ -10,7 +10,10 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
+
 public class AnlzFile {
+	//Initialize Stuff
 	File file = null;
 	File file1 = null;
 	FileReader fr;
@@ -32,65 +35,71 @@ public class AnlzFile {
 	ArrayList<String> storeUsers = new ArrayList<String>();
 	ArrayList<String> matchedWords = new ArrayList<String>();
 
-	public AnlzFile(String filename) {
-		file = new File(filename);
+	//constructor that assigns a post file and a username file
+	public AnlzFile(String postsfile, String usersfile) {
+		file = new File(postsfile);
+		file1 = new File(usersfile);
 	}
-
-	public AnlzFile(String filename, String filename1) {
-		file = new File(filename);
-		file1 = new File(filename1);
-	}
-
+	
+	//constructor that defines a file
 	public void defineFile(String fileName) {
 		file = new File(fileName);
 	}
 
-	public void userSearch() {
 
+	//the biggon
+	public void userSearch() {
+	
+		//get ready to catch errors
 		try {
-			String line = null;
-			String line1 = null;
-			fr = new FileReader(file);
-			br = new BufferedReader(fr);
+			//Define variables
+			String line = null; //Line that will store the lines from the first file
+			String line1 = null; //Line that will store the lines from the second file
+			fr = new FileReader(file); //file reader for the posts
+			br = new BufferedReader(fr); //buffered reader for the posts
 
 			if (file1 != null) {
-				fr1 = new FileReader(file1);
-				br1 = new BufferedReader(fr1);
+				fr1 = new FileReader(file1); //file reader for the second file (users)
+				br1 = new BufferedReader(fr1); //buffered reader for the second file (users)
 			}
 
-			boolean stay = true;
-			String holdLine = null;
-			boolean stay1 = true;
-			int holdLine1 = 0;
+			boolean stay = true; //Loop variable
+			String holdLine = null; //useful hold variable
+			boolean stay1 = true; //second loop variable
+			int holdLine1 = 0; //stores the users choice numerically
 			do {
 				System.out.printf("The file you are searching is %s\n", file.toString());
 				System.out.println("Would you like to search for a predefined pattern? (Profanity) (Type y or n)");
 				holdLine = scanner.nextLine().toLowerCase();
+
+				//Switch through y or n for if you would like to search through a predefined pattern
 				switch (holdLine) {
 
 				case "y":
 					System.out.println(
 							"What would you like to search for? (Please put the number before the item you would like to search for)");
 					do {
-						System.out.println("[1] Profanity");
+						System.out.println("[1] Profanity"); //if more options arise, I will use an arraylist to display them
 						holdLine1 = scanner.nextInt();
 						switch (holdLine1) {
 
 						case 1:
+							//All of the swearing is a necessary evil to search for profanity
 							System.out.println("You have chosen to look for profanity in the file.");
 							pattern = "(f[eu]*c*k+)|(sh+i*t+)|(cu+n+t+)|\\b-?(ass+)(ed)?-?(hole)?\\b|(di+c+k+)|(tw+a+t+)|\\b(tit)(ies)?(s)?\\b|(b[ie]+t+c+h+)|(he+ll+)(^o)?(hole)?\\b|(god)?(damn)(it)?|(wh+o+r+e)|(ni+g+er)|\\b(c[uo]+c+k+)( *[fs]u*cker)?\\b|-?(piss)-?|(bastard)(o)?|(p+u*s+y+)";
-							stay1 = false;
-							stay = false;
+							stay1 = false; //break second loop
+							stay = false;//break first loop
 							break;
 
 						default:
-							System.out.println("Please display an integer!");
+							System.out.println("Please display an integer!"); //if the person does not put a number repeat the loop
 							break;
 						}
 					} while (stay1);
 					break;
 
 				case "n":
+					//The user does not want to search for a predefined pattern. They would like to define their own 
 					System.out.println("Please put the regular expression you would like to search for:");
 					pattern = scanner.nextLine();
 					stay = false;

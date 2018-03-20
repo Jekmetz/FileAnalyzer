0a71539589a730e41a85fe83b4e57555883aa38b
Jay Kmetz <jekmetz@gmail.com>
2018-03-20 13:56:36 -0400

    Modify Main back to search redditposts.txt first

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
 	public static void main(String[] args) {
-		//AnlzFile redditPosts = new AnlzFile("redditPosts.txt","redditAutors.txt");
-		AnlzFile wordtest = new AnlzFile("wordtest.txt");
+		AnlzFile redditPosts = new AnlzFile("redditPosts.txt","redditAutors.txt");
+		//AnlzFile wordtest = new AnlzFile("wordtest.txt");
 		//AnlzFile testFile = new AnlzFile("testfile.txt");
-		//redditPosts.userSearch();
+		redditPosts.userSearch();
 		//testFile.userSearch();
-		wordtest.userSearch();
+		//wordtest.userSearch();
 	}
 }

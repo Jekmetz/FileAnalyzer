81496fba84dbbd2e4ed74476282b11d6447a4947
Jay Kmetz <jekmetz@gmail.com>
2018-03-20 15:25:03 -0400

    delete testfile.txt and modify Main.java as such

--- Main.java
+++ Main.java
@@ -1,10 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
 		AnlzFile redditPosts = new AnlzFile("redditPosts.txt","redditAutors.txt");
+		//AnlzFile redditPosts = new AnlzFile("redditPosts.txt");
 		//AnlzFile wordtest = new AnlzFile("wordtest.txt");
-		//AnlzFile testFile = new AnlzFile("testfile.txt");
 		redditPosts.userSearch();
-		//testFile.userSearch();
 		//wordtest.userSearch();
 	}
 }

2fbe3ce713ff98e7e8f2c1bdd088b775b5b286ae
Jay Kmetz <jekmetz@gmail.com>
2018-03-21 13:11:00 -0400

    add stats to file

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
-		AnlzFile redditPosts = new AnlzFile("redditPosts.txt","redditAutors.txt");
+		//AnlzFile redditPosts = new AnlzFile("redditPosts.txt","redditAutors.txt");
 		//AnlzFile redditPosts = new AnlzFile("redditPosts.txt");
-		//AnlzFile wordtest = new AnlzFile("wordtest.txt");
-		redditPosts.userSearch();
-		//wordtest.userSearch();
+		AnlzFile wordtest = new AnlzFile("wordtest.txt");
+		//redditPosts.userSearch();
+		wordtest.userSearch();
 	}
 }

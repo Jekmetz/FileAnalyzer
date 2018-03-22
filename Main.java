59b3cde76157eb86fceae5abb56ddf4bdd8add9b
Jay Kmetz <jekmetz@gmail.com>
2018-03-22 07:38:01 -0400

    Change main method back to search through redditposts.txt instead of wordtest.txt

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
 	public static void main(String[] args) {
-		//AnlzFile redditPosts = new AnlzFile("redditPosts.txt","redditAutors.txt");
+		AnlzFile redditPosts = new AnlzFile("redditPosts.txt","redditAutors.txt");
 		//AnlzFile redditPosts = new AnlzFile("redditPosts.txt");
-		AnlzFile wordtest = new AnlzFile("wordtest.txt");
+		//AnlzFile wordtest = new AnlzFile("wordtest.txt");
 		//redditPosts.userSearch();
 		wordtest.userSearch();
 	}
